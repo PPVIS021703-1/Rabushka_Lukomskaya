39fe28a06cd901f00dfe8c12be77e99d423ab723
Alisssa <72038770+AlesyaRabushka@users.noreply.github.com>
2021-12-28 17:55:03 +0300

    Update GnomeSort.cpp

--- Laba4/GnomeSort.cpp
+++ Laba4/GnomeSort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 	
-	int a[5] = { 6, 8, 3, 9, 11 };
+	int a[5] = { 6, 8, 3, 9, 11. 5 };
 	//double a[6] = { 2.1, 4.5, 7.3, 4.05, 2.16, 7.2 };
 	//vector<int> a = {6, 2, 5, 1, 3, 11};
 	//vector<double> a = {4.3, 4.7, 5.9, 2.3, 2.4};
